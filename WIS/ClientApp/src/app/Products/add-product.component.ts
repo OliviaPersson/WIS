a316036f601c9673e8ee93149ff6c844c3d187e4
OliviaPersson <olivia__p@hotmail.com>
2022-01-14 15:50:27 +0100

    fixed input fields in add products

--- WIS/ClientApp/src/app/Products/add-product.component.ts
+++ WIS/ClientApp/src/app/Products/add-product.component.ts
@@ -69,5 +69,12 @@ get f() { return this.productService.productForm.controls; }
       }
     });
   }   
+
+  isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+  }  
 }
   
